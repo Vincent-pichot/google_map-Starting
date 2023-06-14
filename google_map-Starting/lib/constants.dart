3735814ea7b7e48d20a024f9e3a7dc2611efcffa
Vincent-pichot <76951490+Vincent-pichot@users.noreply.github.com>
2023-06-14 09:30:28 +0700

    Initial commit

---
+++ google_map-Starting/lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyA301BywgIM7sHfWHHD2NgalcFuCKVOOPo";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
---
+++ google_map-Starting/lib/order_traking_page.dart
@@ -0,0 +1,171 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+//import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_mao/constants.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class OrderTrackingPage extends StatefulWidget {
+  const OrderTrackingPage({Key? key}) : super(key: key);
+
+  @override
+  State<OrderTrackingPage> createState() => OrderTrackingPageState();
+}
+
+class RequestReceiver {
+  final String latitude;
+  final String longitude;
+
+  RequestReceiver({required this.latitude, required this.longitude});
+
+  factory RequestReceiver.fromJson(Map<String, dynamic> json) {
+    return RequestReceiver(
+        latitude: json['latitude'], longitude: json['longitude']);
+  }
+}
+
+class OrderTrackingPageState extends State<OrderTrackingPage> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  LatLng destination = LatLng(37.33429383, -122.06600055);
+
+  List<LatLng> polylineCoordinates = [];
+  LocationData? currentLocation;
+
+  BitmapDescriptor currentLocationIcon = BitmapDescriptor.defaultMarker;
+  BitmapDescriptor destinationIcon = BitmapDescriptor.defaultMarker;
+
+  void getCurrentLocation() async {
+    Location location = Location();
+
+    location.getLocation().then(
+      (location) {
+        currentLocation = location;
+      },
+    );
+
+    GoogleMapController googleMapController = await _controller.future;
+
+    location.onLocationChanged.listen(
+      (newLoc) {
+        currentLocation = newLoc;
+
+        googleMapController.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+                zoom: 13.5,
+                target: LatLng(
+                  newLoc.latitude!,
+                  newLoc.longitude!,
+                )),
+          ),
+        );
+
+        setState(() {});
+      },
+    );
+  }
+
+/*
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        google_api_key,
+        PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+        PointLatLng(sourceLocation.latitude, destination.longitude));
+
+    if (result.points.isNotEmpty) {
+      result.points.forEach(
+        (PointLatLng point) =>
+            polylineCoordinates.add(LatLng(point.latitude, point.longitude)),
+      );
+      setState(() {});
+    }
+  }
+*/
+
+  void setCustomMarkerIcon() {
+    BitmapDescriptor.fromAssetImage(
+            ImageConfiguration.empty, "assets/Pin_current_location.png")
+        .then((icon) {
+      currentLocationIcon = icon;
+    });
+
+    BitmapDescriptor.fromAssetImage(
+            ImageConfiguration.empty, "assets/Pin_destination.png")
+        .then((icon) {
+      destinationIcon = icon;
+    });
+  }
+
+  @override
+  void initState() {
+    getData();
+    getCurrentLocation();
+    setCustomMarkerIcon();
+    //getPolyPoints();
+    super.initState();
+  }
+
+  Future getData() async {
+    var url = 'https://ourpetsgps.000webhostapp.com/get.php';
+    var response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      var data = RequestReceiver.fromJson(jsonDecode(response.body));
+      destination =
+          LatLng(double.parse(data.latitude), double.parse(data.latitude));
+      print(destination);
+    }
+    print(response.statusCode);
+    // print(data.toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Track order",
+          style: TextStyle(color: Colors.black, fontSize: 16),
+        ),
+      ),
+      body: currentLocation == null
+          ? const Center(child: Text("Loading"))
+          : GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: LatLng(
+                    currentLocation!.latitude!, currentLocation!.longitude!),
+                zoom: 13.5,
+              ), //CameraPosition
+              polylines: {
+                Polyline(
+                  polylineId: const PolylineId("route"),
+                  points: polylineCoordinates,
+                  color: primaryColor,
+                  width: 6,
+                ),
+              }, //Polylines
+              markers: {
+                Marker(
+                    markerId: const MarkerId("currentLocation"),
+                    icon: currentLocationIcon,
+                    position: LatLng(currentLocation!.latitude!,
+                        currentLocation!.longitude!)),
+                Marker(
+                  markerId: const MarkerId("destination"),
+                  icon: destinationIcon,
+                  position: destination,
+                ),
+              }, //Markers
+              onMapCreated: (mapController) {
+                _controller.complete(mapController);
+              },
+            ),
+    );
+  }
+}
